68d6269b3bcb89ad51fa3dd34fba8c4247f126c2
SwapnilPorwal <Swapnilporwal2013@gmail.com>
2018-05-06 13:42:17 +0530

    Updated Facebook_TestRunner.java

--- CucumberTestNG/src/test/java/runners/Facebook_TestRunner.java
+++ CucumberTestNG/src/test/java/runners/Facebook_TestRunner.java
@@ -3,10 +3,9 @@ package runners;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features= "C:\\WorkSpace\\CucumberTestNG\\src\\test\\resources\\FeatureFiles\\FacebookLogin.feature",
+@CucumberOptions(features= "C:\\Users\\DELL\\git\\Cucumber\\CucumberTestNG\\src\\test\\resources\\FeatureFiles\\FacebookLogin.feature",
 					glue= {"stepDefinitions"},
 					dryRun = false,
-					tags= {"@Automation"},
 					monochrome=true,
 					strict =true)
 public class Facebook_TestRunner extends AbstractTestNGCucumberTests{
